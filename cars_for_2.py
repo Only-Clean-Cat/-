fce29f7276a68d30010415fb38bf44b0e9508bd5
Дмитрий <160622322+Only-Clean-Cat@users.noreply.github.com>
2024-02-20 18:50:53 +0300

    Add files via upload
    Домашнее задание к урокам 
    "Стиль кода часть II. Цикл While."
    "Цикл for. Элементы списка. Полезные функции в цикле"

---
+++ cars_for_2.py
@@ -0,0 +1,8 @@
+my_cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for car in my_cars:
+    print('Я еду на автомобиле марки: ', car)
+    cars_count += 10
+    print(cars_count)
+print('ПОМЧАЛИ!')
+print(cars_count)
