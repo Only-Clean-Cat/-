70985c9a4fab8e89a55935f676c0bc7b69e0ec2b
Дмитрий <160622322+Only-Clean-Cat@users.noreply.github.com>
2024-02-21 16:34:30 +0300

    Delete cars_for_2.py

--- cars_for_2.py
+++
@@ -1,8 +0,0 @@
-my_cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
-cars_count = 0
-for car in my_cars:
-    print('Я еду на автомобиле марки: ', car)
-    cars_count += 10
-    print(cars_count)
-print('ПОМЧАЛИ!')
-print(cars_count)
