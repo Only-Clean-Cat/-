8fcdc2418c1fe436b40c9d9125d1d21c1ba473da
Дмитрий <160622322+Only-Clean-Cat@users.noreply.github.com>
2024-02-24 21:00:32 +0300

    Add files via upload
    Домашняя работа по уроку "Пространство имен и способы вызова функции"

---
+++ function_arg.py
@@ -0,0 +1,16 @@
+def test():
+    a, b = 10, 20
+    print('a =', a, ',', 'b =', b)
+
+
+test()
+
+
+def test2(param1, param2, param3):
+    res = param1+' '+param2+' '+param3
+    print(res)
+
+test2('I learn', 'Python', 'every day!')
+
+
+
